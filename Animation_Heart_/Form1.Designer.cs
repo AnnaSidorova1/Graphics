b522b866b8e9b53c3149079a5b9da71c60cab4b7
Anna Sidorova <60873654+AnnaSidorova1@users.noreply.github.com>
2021-03-22 17:51:59 +0300

    Animation on heart

---
+++ Animation_Heart_/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace Heart
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zgc = new ZedGraph.ZedGraphControl();
+            this.SuspendLayout();
+            // 
+            // zgc
+            // 
+            this.zgc.Location = new System.Drawing.Point(73, 29);
+            this.zgc.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zgc.Name = "zgc";
+            this.zgc.ScrollGrace = 0D;
+            this.zgc.ScrollMaxX = 0D;
+            this.zgc.ScrollMaxY = 0D;
+            this.zgc.ScrollMaxY2 = 0D;
+            this.zgc.ScrollMinX = 0D;
+            this.zgc.ScrollMinY = 0D;
+            this.zgc.ScrollMinY2 = 0D;
+            this.zgc.Size = new System.Drawing.Size(600, 600);
+            this.zgc.TabIndex = 0;
+            this.zgc.UseExtendedPrintDialog = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 657);
+            this.Controls.Add(this.zgc);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zgc;
+    }
+}
+
---
+++ Animation_Heart_/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace Heart
+{
+    public partial class Form1 : Form
+    {
+        List<double[]> Points = new List<double[]>();
+
+        private System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer() { Interval = 1000 };
+
+        public Form1()
+        {
+            //отрисовка сердца и старт таймера
+            InitializeComponent();
+            DrawHeart();
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+
+        private void DrawHeart()
+        {
+            GraphPane pane = zgc.GraphPane;
+            pane.YAxis.IsVisible = false;
+
+            pane.CurveList.Clear();
+
+            //координаты отображаемой области
+            pane.XAxis.Scale.Min = -15;
+            pane.XAxis.Scale.Max = 15;
+            pane.YAxis.Scale.Min = -15;
+            pane.YAxis.Scale.Max = 15;
+            zgc.AxisChange();
+
+            //фигура
+            PointPairList list = new PointPairList();
+            double[] point1 = new double[] { 0, 3, 1 };
+            double[] point2 = new double[] { 5, 8, 1 };
+            double[] point3 = new double[] { 9, 8, 1 };
+            double[] point4 = new double[] { 10, 3, 1 };
+            double[] point5 = new double[] { 7, -3, 1 };
+            double[] point6 = new double[] { 0, -11, 1 };
+            double[] point7 = new double[] { -7, -3, 1 };
+            double[] point8 = new double[] { -10, 3, 1 };
+            double[] point9 = new double[] { -9, 8, 1 };
+            double[] point10 = new double[] { -5, 8, 1 };
+            double[] point11 = new double[] { 0, 3, 1 };
+
+            Points.Add(point1);
+            Points.Add(point2);
+            Points.Add(point3);
+            Points.Add(point4);
+            Points.Add(point5);
+            Points.Add(point6);
+            Points.Add(point7);
+            Points.Add(point8);
+            Points.Add(point9);
+            Points.Add(point10);
+            Points.Add(point11);
+
+            for (int i = 0; i < 11; ++i)
+            {
+                list.Add(Points[i][0], Points[i][1]);
+            }
+
+            LineItem Krug = pane.AddCurve("", list, Color.Red, SymbolType.None);
+            Krug.Line.Style = System.Drawing.Drawing2D.DashStyle.Solid;
+            Krug.Line.IsSmooth = true;
+
+        }
+
+        private double[] Multiplication_Matrix(double[] vector, double[,] matrix)
+        {
+            //умножение вектора на матрицу
+            double[] newvector = new double[3];
+            double tmp = 0;
+            for (int i = 0; i < 3; ++i)
+            {
+                for (int j = 0; j < 3; ++j)
+                {
+                    tmp += vector[j] * matrix[i, j];
+                }
+                newvector[i] = tmp;
+                tmp = 0;
+            }
+            return newvector;
+        }
+
+        private void Move()
+        {
+            GraphPane pane = zgc.GraphPane;
+            pane.YAxis.IsVisible = false;
+            zgc.Invalidate();
+            pane.CurveList.Clear();
+
+            //координаты отображаемой области
+            pane.XAxis.Scale.Min = -15;
+            pane.XAxis.Scale.Max = 15;
+            pane.YAxis.Scale.Min = -15;
+            pane.YAxis.Scale.Max = 15;
+            zgc.AxisChange();
+
+            //увеличение всех координат в 1.2 раза
+            PointPairList list = new PointPairList();
+
+            double[,] matrix = { { 1.2, 0, 0 }, { 0, 1.2, 0 }, { 0, 0, 1 } };
+
+            for (int i = 0; i < 11; ++i)
+            {
+                Points[i] = Multiplication_Matrix(Points[i], matrix);
+            }
+
+            for (int i = 0; i < 11; ++i)
+            {
+                list.Add(Points[i][0], Points[i][1]);
+            }
+
+            LineItem Krug = pane.AddCurve("", list, Color.Red, SymbolType.None);
+            Krug.Line.Style = System.Drawing.Drawing2D.DashStyle.Solid;
+            Krug.Line.IsSmooth = true;
+            this.Refresh();
+        }
+
+        private void Remove()
+        {
+            GraphPane pane = zgc.GraphPane;
+            pane.YAxis.IsVisible = false;
+            zgc.Invalidate();
+            pane.CurveList.Clear();
+
+            //координаты отображаемой области
+            pane.XAxis.Scale.Min = -15;
+            pane.XAxis.Scale.Max = 15;
+            pane.YAxis.Scale.Min = -15;
+            pane.YAxis.Scale.Max = 15;
+            zgc.AxisChange();
+
+            //уменьшение всех координат в 1.2 раза
+            PointPairList list = new PointPairList();
+
+            double[,] matrix = { { 1/1.2, 0, 0 }, { 0, 1/1.2, 0 }, { 0, 0, 1 } };
+
+            for (int i = 0; i < 11; ++i)
+            {
+                Points[i] = Multiplication_Matrix(Points[i], matrix);
+            }
+            for (int i = 0; i < 11; ++i)
+            {
+                list.Add(Points[i][0], Points[i][1]);
+            }
+            LineItem Krug2 = pane.AddCurve("", list, Color.Red, SymbolType.None);
+            Krug2.Line.Style = System.Drawing.Drawing2D.DashStyle.Solid;
+            Krug2.Line.IsSmooth = true;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            //вызов перерисовки и события таймера
+            this.Refresh();
+            Thread.Sleep(100);
+            Move();
+            Thread.Sleep(100);
+            Remove();
+            Thread.Sleep(100);
+        }
+    }
+}
